babde1dcb0347517332bf0250fc642539f40da25
grodriguezcontextglobal <grodriguez@contextglobal.com>
2023-08-31 18:44:34 -0400

    fixing some bugs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,11 +12,12 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
+    'react-hooks/exhaustive-deps': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
-    "react/prop-types": 0,
-    "no-unused-vars": "off"
+    'react/prop-types': 0,
+    'no-unused-vars': 'off'
   },
 }
