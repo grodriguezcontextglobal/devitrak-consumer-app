d98badc9b3984be003e21e59165606516257a01a
grodriguezcontextglobal <grodriguez@contextglobal.com>
2023-08-31 23:08:20 -0400

    new way to deploy w/gh

--- src/main.jsx
+++ src/main.jsx
@@ -15,7 +15,7 @@ const persistor = persistStore(store);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <React.StrictMode>
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.DEV ? '/' : '/devitrak-consumer-app/'}>
         <PersistGate persistor={persistor}>
             <QueryClientProvider client={queryClient}>
               <App />
