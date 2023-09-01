4c0aa2d57e05d1a33b159ce20f5fff9d11ffa1c6
grodriguezcontextglobal <grodriguez@contextglobal.com>
2023-09-01 10:59:29 -0400

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
+// vite.config.js
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+    serve:{
+      port: 3791
+    }
+  }
+
+  if (command !== 'serve') {
+    config.base = '/devitrak-consumer-app/'
+  }
+
+  return config
 })
